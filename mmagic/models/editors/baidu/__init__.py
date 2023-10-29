68a0dd6a2ac341828b66b3c1bdea2c1027d55211
tripleMu <gpu@163.com>
2023-10-29 13:59:43 +0800

    fix code

--- mmagic/models/editors/baidu/__init__.py
+++ mmagic/models/editors/baidu/__init__.py
@@ -1,7 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .repvgg_unet import RepVGGUnet
 
-__all__ = [
-    'RepVGGUnet', 'UNetBaiduDFL', 'ReconstructiveSubNetwork',
-    'ReconstructiveSubNetworkRepVGG'
-]
+__all__ = ['RepVGGUnet']
