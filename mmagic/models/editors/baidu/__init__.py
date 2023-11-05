dba6c804da28d2b54505e93ea5b4bcbf3ea7ebcc
tripleMu <gpu@163.com>
2023-11-05 21:31:17 +0800

    add v6

--- mmagic/models/editors/baidu/__init__.py
+++ mmagic/models/editors/baidu/__init__.py
@@ -3,5 +3,9 @@ from .repvgg_unet import RepVGGUnet
 from .repvgg_unet_v2 import RepVGGUnetV2
 from .repvgg_unet_v3 import RepVGGUnetV3
 from .repvgg_unet_v4 import RepVGGUnetV4
+from .repvgg_unet_v6 import RepVGGUnetV6
 
-__all__ = ['RepVGGUnet', 'RepVGGUnetV2', 'RepVGGUnetV3', 'RepVGGUnetV4']
+__all__ = [
+    'RepVGGUnet', 'RepVGGUnetV2', 'RepVGGUnetV3', 'RepVGGUnetV4',
+    'RepVGGUnetV6'
+]
