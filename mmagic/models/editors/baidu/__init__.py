2de373b17bca311acc124e5750ec1dbb217ac421
tripleMu <gpu@163.com>
2023-11-16 17:08:45 +0800

    add final

--- mmagic/models/editors/baidu/__init__.py
+++ mmagic/models/editors/baidu/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .conv_sr_net import ConvSRNet
+from .repvgg_final import RepVGGUnetFinal
 from .repvgg_unet import RepVGGUnet
 from .repvgg_unet_v2 import RepVGGUnetV2
 from .repvgg_unet_v3 import RepVGGUnetV3
@@ -9,5 +10,5 @@ from .repvgg_unet_v7 import RepVGGUnetV7
 
 __all__ = [
     'RepVGGUnet', 'RepVGGUnetV2', 'RepVGGUnetV3', 'RepVGGUnetV4',
-    'RepVGGUnetV6', 'RepVGGUnetV7', 'ConvSRNet'
+    'RepVGGUnetV6', 'RepVGGUnetV7', 'ConvSRNet', 'RepVGGUnetFinal'
 ]
